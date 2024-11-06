ba1dea456dbeadbd35e2f43e09199790b5e3ca2a
LsRood26 <carloschavezr26@gmail.com>
2024-11-06 00:00:34 -0500

    feat: Role model and constants

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -1,24 +1,37 @@
+import 'package:visitorregistration/models/role.dart';
+
 class User {
   final String dni;
   final String name;
   final String lastName;
-  final int role;
+  final Role role;
+  final String accessToken;
 
   User(
       {required this.dni,
       required this.name,
       required this.lastName,
-      required this.role});
+      required this.role,
+      required this.accessToken});
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
         dni: map['dni'],
         name: map['name'],
         lastName: map['lastname'],
-        role: map['roleId']);
+        role: Role.fromMap(
+          map['role'],
+        ),
+        accessToken: map['access_token']);
   }
 
   Map<String, dynamic> toMap() {
-    return {'dni': dni, 'name': name, 'lastname': lastName, 'roleId': role};
+    return {
+      'dni': dni,
+      'name': name,
+      'lastname': lastName,
+      'role': role.toMap(),
+      'access_token': accessToken
+    };
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  final ID_RESIDENTE = 1;
+  final ID_VISITANTE = 2;
+  final ACCEPTED = 'accepted';
+  final REJECTED = 'rejected';
+  final PENDING = 'pending';
+}
